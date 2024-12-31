80484ea76ac0cba09339a89b0cbf70662cdb4800
Chris McLean <mclean.webdev@gmail.com>
2024-12-30 21:40:12 -0700

    use dotenv in knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 // Update with your config settings.
 
 /**
@@ -7,11 +11,11 @@ export default {
   development: {
     client: 'mysql2',
     connection: {
-      database: 'spellbook_api',
-      host: 'localhost',
-      port: 3306,
-      user: 'root',
-      password: ''
+      database: process.env.DB_NAME ?? '',
+      host: process.env.DB_HOST ?? '',
+      port: process.env.DB_PORT ?? '',
+      user: process.env.DB_USERNAME ?? '',
+      password: process.env.DB_PASSWORD ?? ''
     },
     migrations: {
       tableName: 'knex_migrations'
